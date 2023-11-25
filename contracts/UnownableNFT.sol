fd8280835cfc2f12741a438b93a656187fc35731
Tadej Vengust <vengust.tadej@gmail.com>
2023-11-25 12:01:53 +0100

    Add unownable NFT logic.

---
+++ contracts/UnownableNFT.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract UnownableNft is ERC721 {
+
+  /**
+   * This collection can only have 1 NFT.
+   */
+  string public _tokenURI;
+  
+  /**
+   * @param _name collection name
+   * @param _symbol collection symbol
+   * @param _uri metadata uri
+   */
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _uri
+  ) ERC721(_name, _symbol) {
+    _safeMint(msg.sender, 1);
+    _tokenURI = _uri;
+  }
+
+  /**
+   * Take possesion of the Unownable NFT.
+   */
+  function takePossession()
+    external
+  {
+    require(ownerOf(1) != msg.sender, "You already have NFT in possession.");
+    _update(msg.sender, 1, address(0));
+
+  }
+
+  /**
+   * @dev Get token uri
+   * @param tokenId nft id
+   */
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+   _requireOwned(tokenId);
+    return _tokenURI;
+  }
+}
--- test/UnownableNFT.js
+++ test/UnownableNFT.js
@@ -1,8 +1,31 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("UnownableNFT", function () {
-  it("test", async function () {
-    console.log("test");
+describe("UnownableNft", function () {
+  let UnownableNft, account1, account2;
+
+  beforeEach(async () => {
+    [account1, account2] = await ethers.getSigners();
+
+    const UnownableNftContract = await ethers.getContractFactory(
+      "UnownableNft"
+    );
+    UnownableNft = await UnownableNftContract.deploy(
+      "Unownable NFT",
+      "UNFT",
+      "http://example.com/"
+    );
+    await UnownableNft.deployed();
+  });
+
+  it("Check current possessor.", async function () {
+    const possessor = await UnownableNft.ownerOf(1);
+    expect(possessor).to.be.equal(account1.address);
+  });
+
+  it("Takes possession of the NFT.", async function () {
+    await UnownableNft.connect(account2).takePossession();
+    const possessor = await UnownableNft.ownerOf(1);
+    expect(possessor).to.be.equal(account2.address);
   });
 });
